31484353eea7eeb3865d826e761ef27b793cee11
rupak2007 <rupakraj.v@gmail.com>
2026-03-25 15:17:55 +0530

    Rupak Added Use Case 1: Application Entry & Welcome Message

---
+++ App/src/BookMyStayApp.java
@@ -0,0 +1,27 @@
+/**
+ * UseCase1HotelBookingApp
+ *
+ * This class represents the entry point of the Hotel Booking Application.
+ * It prints a welcome message along with application name and version.
+ *
+ * @author Rupak Raj
+ * @version 1.0
+ */
+public class UseCase1HotelBookingApp {
+
+    /**
+     * Main method - Entry point of the application
+     * @param args Command line arguments
+     */
+    public static void main(String[] args) {
+
+        // Welcome message
+        System.out.println("=======================================");
+        System.out.println("   Welcome to Book My Stay App 🏨");
+        System.out.println("   Hotel Booking System v1.0");
+        System.out.println("=======================================");
+        System.out.println("Your comfort is our priority!");
+        System.out.println("Application started successfully.");
+
+    }
+}
